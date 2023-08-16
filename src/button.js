16889444bcad7949be07085f7f3ef2aa575791de
c-dez <hdezcruzcarlos@gmail.com>
2023-08-15 21:44:53 -0600

    added buton.js

---
+++ src/button.js
@@ -0,0 +1,34 @@
+ function BtnObj(name,value){
+    this.name = name;
+    this.value = value;
+    this.fun = function()
+    {
+        console.log(`${this.name} ${this.value}`)
+    }
+}
+
+
+const boton = (name, value)=>
+{
+    const obj = new BtnObj(name, value)
+
+    const element = document.createElement('button');
+    element.classList.add(`btn${obj.value}`)
+    element.textContent = obj.name;
+    element.style.height = '100px'
+    element.style.width = '100px'
+
+    element.style.borderRadius = '50%'
+    element.style.border = '3px solid white'
+    element.style.backgroundColor = '#f3ea72'
+    element.style.color = 'black'
+    element.style.fontSize = '18px'
+
+    return element;
+
+}
+
+
+
+export {boton}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+import {  boton } from "./button.js";
+
+
+const botonesContainer = document.querySelector('.botones')
+const btn10 = boton('$10', '10')
+const btn50 = boton('$50','50')
+const btn100 = boton('$100', '100')
+const btn500 = boton('$500', '500')
+
+botonesContainer.appendChild(btn10)
+botonesContainer.appendChild(btn50)
+botonesContainer.appendChild(btn100)
+botonesContainer.appendChild(btn500)
+
+
+
